e5ad0230a9b78d2f5c49cc0c9f502aa9fc469eae
Quân Võ <minhquan240204@gmail.com>
2025-10-13 22:06:50 +0700

    Danh sách sản phẩm

---
+++ src/app/[locale]/products/page.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import MainLayout from '@/components/layout/MainLayout';
+
+const products = [
+  
+];
+
+export default function ProductPage() {
+  return (
+    <MainLayout>
+      <div className="py-8 px-4">
+        <h1 className="text-2xl font-bold mb-6 text-center">Danh sách sản phẩm</h1>
+      </div>
+    </MainLayout>
+  );
+}
